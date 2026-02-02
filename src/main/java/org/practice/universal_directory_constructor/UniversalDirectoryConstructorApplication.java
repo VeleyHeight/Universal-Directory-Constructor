a9eb50c9ac764c697e203c04826647f2a90be267
VeleyHeight <niki2606@bk.ru>
2026-02-02 14:38:53 +0300

    Start project

---
+++ src/main/java/org/practice/universal_directory_constructor/UniversalDirectoryConstructorApplication.java
@@ -0,0 +1,13 @@
+package org.practice.universal_directory_constructor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalDirectoryConstructorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversalDirectoryConstructorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/practice/universal_directory_constructor/UniversalDirectoryConstructorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.universal_directory_constructor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalDirectoryConstructorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
